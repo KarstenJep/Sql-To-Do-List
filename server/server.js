e619bdcd29e67d391501002394536fa934aac39b
Karsten Jepsen <Kjepsen86@gmail.com>
2021-04-24 09:19:42 -0500

    Setup server.js

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const taskRouter = require('./routes/task.router.js');
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/todo', taskRouter);
+
+app.use(express.static('server/public'))
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log('listening on port', PORT);
+});
