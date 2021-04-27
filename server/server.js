1a069a914c7a62a2d78b1ba404f9cab66e00305f
Karsten Jepsen <76796887+KarstenJep@users.noreply.github.com>
2021-04-26 19:21:16 -0500

    Revert "Feature confirm delete"

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
-// requires
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const app = express();
 
 // Setup body parser
-app.use(bodyParser.urlencoded({extended: true})); // should be loaded before routes
+app.use(bodyParser.urlencoded({extended: true}));
 app.use( bodyParser.json());
 app.use(express.static('server/public'));
 
@@ -12,7 +12,7 @@ app.use(express.static('server/public'));
 const taskRouter = require('./routes/task_router.js');
 app.use('/todo', taskRouter);
 
-// Setup port for heroku - will get the # from process environment variables
+// Start express
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log('listening on port', PORT);
